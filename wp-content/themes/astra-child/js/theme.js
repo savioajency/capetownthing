084ffa4486e133b04095b5fe91a1c5e6b24d7e45
Savio Menezes <105903521+savioajency@users.noreply.github.com>
2022-06-08 12:37:19 +0530

    Reset contact form on modal window show

--- wp-content/themes/astra-child/js/theme.js
+++ wp-content/themes/astra-child/js/theme.js
@@ -2,6 +2,8 @@ var $ = jQuery.noConflict();
 
 $(document).ready(function(){
     $(".book_btn").on('click',function(){
+        $('#bookingModal form').trigger("reset");
+        $('#bookingModal form .wpcf7-response-output').html('');
         
         var post_id = $(this).closest(".booking_div").find("[name='list_post_id']").val();
         var post_name = $(this).closest(".booking_div").find("[name='list_post_name']").val();
