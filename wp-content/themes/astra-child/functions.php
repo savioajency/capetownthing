86baee0e44542af5ca72ba97c42644cf8f6105d1
Savio Menezes <105903521+savioajency@users.noreply.github.com>
2022-06-08 17:35:26 +0530

    Redirect to external website if user visits to single landing page

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -50,3 +50,32 @@ add_action("wp_footer",function(){
 </div>
 <?php
 });
+
+//if page visted is single listing post type then redirect to its external website
+//if external website link not there then redirect to homepage
+add_action('template_redirect', function() {
+  if (is_singular('listing')){      
+    $post_id = get_queried_object_id();
+    $website_link = fetch_website_link_by_id($post_id);
+    
+    if(empty($website_link)){
+        $website_link = get_site_url();
+    }
+    
+    wp_redirect($website_link);
+    exit;
+  }
+});
+
+function fetch_website_link_by_id($post_id){
+    global $wpdb;
+    $website_link="";
+    $query = "SELECT value FROM pantheon.wp_ulisting_listing_attribute_relationships where listing_id=".$post_id." and attribute='website_link'";
+    
+    $result = $wpdb->get_results($query,ARRAY_A);
+    if(!empty($result)){
+        $website_link = $result[0]['value'];
+    }
+    
+    return $website_link;
+}
